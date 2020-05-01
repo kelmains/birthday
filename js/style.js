b912ce44c9ba9b6d288dc5127ace69f9f76db91f
Kelvin Maina <kelmains@gmail.com>
2020-05-01 09:44:30 +0300

    added arrays

---
+++ js/style.js
@@ -0,0 +1,3 @@
+var days =['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
+var mNames =['Kwasi', 'Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var fNames = ['Akosua', 'Adwoa', 'Abenaa','Akua', 'Yaa','Afua'];
